953d9db683c3050b0318634198a3c008395b84b8
Ainur Rakhmanov <ainur-205@yandex.ru>
2022-11-07 20:38:21 +0300

    Task 7 complete

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,6 @@
 FROM tiangolo/uvicorn-gunicorn-fastapi:latest
 
+COPY pyproject.toml ./
+RUN pip install poetry --no-cache-dir && poetry config virtualenvs.create false && poetry install --no-dev
+
 COPY main.py /app/main.py
--- main.py
+++ main.py
@@ -1,25 +1,53 @@
 import os
+import datetime
+import json
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Header, Request
 from fastapi.responses import HTMLResponse
+
+from redis import Redis
 
 import uvicorn
 
 app = FastAPI()
 
-counter = 0
+redis = Redis(
+    host=os.getenv("REDIS_HOST", "redis"), port=int(os.getenv("REDIS_PORT", 6379))
+)
+
+redis.set("counter", 0)
+
+
+@app.middleware("http")
+async def log_visits(req: Request, call_next):
+    res = await call_next(req)
+    redis.lpush(
+        "counter visits",
+        json.dumps(
+            {
+                "User-Agent": req.headers.get("User-Agent"),
+                "Date": str(datetime.datetime.utcnow()),
+                "Path": req.url.path,
+            }
+        ),
+    )
+    return res
 
 
 @app.get("/")
 def get_counter():
+    counter = int(redis.get("counter"))
     return counter
+
+
+@app.get("/visits")
+def get_visits():
+    return [json.loads(i) for i in redis.lrange("counter visits", 0, -1)]
 
 
 @app.get("/stat")
 def get_counter():
-    global counter
-    counter += 1
-    return counter
+    return redis.incr("counter")
 
 
 @app.get("/about")
@@ -27,5 +55,5 @@ def hello():
     return HTMLResponse("<h3>Hello, ainur</h3>")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     uvicorn.run(app, port=int(os.getenv("PORT", 5000)))
